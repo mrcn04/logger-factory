c6167a703c598f737cd8c4affd1857d59e654b10
mrcn04 <ocengiz97@hotmail.com>
2021-12-27 02:36:09 +0300

    added test

--- src/factory-class-test.ts
+++
@@ -1,7 +0,0 @@
-import { logger } from './factory-class';
-
-logger.info("Info text");
-logger.debug("Debug text");
-logger.warn("Warn text");
-logger.error("Error text");
-logger.log('Log text with multiple values', 1)
---
+++ src/factory-class.test.ts
@@ -0,0 +1,25 @@
+import { logger } from './factory-class'
+
+function logEverything() {
+  logger.info("Info text"),
+  logger.debug("Debug text"),
+  logger.warn("Warn text"),
+  logger.error("Error text"),
+  logger.log('Log text with multiple values', 1)
+}
+
+test('logs all five', () => {
+  const info = jest.spyOn(console, 'info');
+  const debug = jest.spyOn(console, 'debug');
+  const warn = jest.spyOn(console, 'warn');
+  const error = jest.spyOn(console, 'error');
+  const log = jest.spyOn(console, 'log');
+  logEverything()
+  expect(info).toHaveBeenCalled()
+  expect(debug).toHaveBeenCalled()
+  expect(warn).toHaveBeenCalled()
+  expect(error).toHaveBeenCalled()
+  expect(log).toHaveBeenCalled()
+})
+
+
